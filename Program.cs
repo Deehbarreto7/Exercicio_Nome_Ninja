d4dc9d5c8932f7c16026278a4f0912d6600b04a4
Debora <deehbarreto7@gmail.com>
2026-04-05 22:58:20 -0300

    exercicio do nome ninja

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Write("---Nome Ninja---");
+Console.Write("Digite seu nome");
+string nome = Console.ReadLine()!.ToUpper()!.ToLower();
+
+string nomeNinja = nome
+ .Replace("a","KA")
+    .Replace("b","ZU") 
+    .Replace("c","MI")
+    .Replace("d","TE")
+    .Replace("e","KU")
+    .Replace("f","LU")
+    .Replace("g","JI")
+    .Replace("h","RI")
+    .Replace("i","KI")
+    .Replace("j","ZUS")
+    .Replace("k","ME")
+    .Replace("l","TA")
+    .Replace("m","RIN")
+    .Replace("n","TO")
+    .Replace("o","MO")
+    .Replace("p","NO")
+    .Replace("q","KE")
+    .Replace("r","SHI")
+    .Replace("s","ARI")
+    .Replace("t","CHI")
+    .Replace("u","DO")
+    .Replace("v","RU")
+    .Replace("w","MEI")
+    .Replace("x","NA")
+    .Replace("y","FU")
+    .Replace("z","ZI");
+
+nomeNinja = char.ToUpper(nomeNinja[0]) + nomeNinja.Substring();
+
+Console.WriteLine("Seu nome ninja é:" + nomeNinja);
+
+
+
+
+
+
+
+ 
